0a0c40498fc724a4110c8d435c5e4776fce158ed
Santiago Souto <ssout@dolby.com>
2023-06-06 00:53:32 -0300

    Add bdd mongo support with models created

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const bcrypt = require('bcrypt-nodejs')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    email: {type: String, unique: true, lowercase: true, required: true},
+    password: {type: String, required: true},
+    subjects: {type: [Schema.Types.ObjectId], default: []}
+}, {
+    timestamps: true
+})
+
+// encrypt password before saving
+userSchema.pre('save', function(next) {
+    const user = this;
+    // User exists but did not change password
+    if (!user.isModified('password')) {
+        return next();
+    }
+
+    // Generate salt for preventing dict attacks
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            next(err);
+        }
+        bcrypt.hash(user.password, salt, null, (err, hash) => {
+            if (err) {
+                next(err);
+            }
+            user.password = hash;
+            next();
+        })
+    })
+})
+
+userSchema.methods.comparePassword = function (password, callback) {
+    bcrypt.compare(password, this.password, (err, theyMatch) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, theyMatch);
+    })
+}
+
+userSchema.methods.addSubject = function (subject, callback) {
+    this.subjects = [...this.subjects, subject];
+    this.save().then(() => callback(null, 'Success'));
+}
+
+module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,19 +1,54 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
+const express = require('express');
+const session = require('express-session')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const MongoStore = require('connect-mongo');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const passportConfig = require('./config/passport.js');
 
-const app = express()
+dotenv.config();
 
 // Constants
 const PORT = 3000;
+const MONGO_URL = `mongodb+srv://umcalendar:${process.env.MONGODB_PASSWORD}@cluster0.wnudgqa.mongodb.net/?retryWrites=true&w=majority`;
+
+const app = express();
+
+// Mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (err) => {
+    throw err;
+    process.exit(1);
+})
 
 // Install middlewares
+app.use(session({
+    secret: "THIS IS A SECRET",
+    resave: true,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: MONGO_URL })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
+})
+
+// User controls
+const userController = require('./controllers/user');
+app.post('/signup', userController.postSignUp);
+app.post('/login', userController.postLogin);
+app.get('/logout', passportConfig.isAuthenticated, userController.logout);
+
+app.get('/userInfo', passportConfig.isAuthenticated, (req, res) => {
+    res.json(req.user);
 })
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
